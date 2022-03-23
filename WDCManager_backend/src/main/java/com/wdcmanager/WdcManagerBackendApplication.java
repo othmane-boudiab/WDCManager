9d69306dc567e1ba90ea143b8400ff8d6072dfa2
othmane-boudiab <mrboudiabothmane@gmail.com>
2022-03-23 09:16:26 +0100

    create project spring boot

---
+++ WDCManager_backend/src/main/java/com/wdcmanager/WdcManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wdcmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WdcManagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WdcManagerBackendApplication.class, args);
+    }
+
+}
---
+++ WDCManager_backend/src/test/java/com/wdcmanager/WdcManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wdcmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WdcManagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
