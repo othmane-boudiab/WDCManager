9c42f3e06bdeb99caa9b332991b072303f78e174
othmane-boudiab <mrboudiabothmane@gmail.com>
2022-04-19 11:19:33 +0000

    add pages

--- WDCManager_frontend/src/app/app.module.ts
+++ WDCManager_frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { UtilisateursComponent } from './pages/utilisateurs/utilisateurs.component';
+import { ActivitiesComponent } from './pages/activities/activities.component';
+import { ExercisesComponent } from './pages/exercises/exercises.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    UtilisateursComponent,
+    ActivitiesComponent,
+    ExercisesComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
