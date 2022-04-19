e040ebbcdd44abd2273733fc243190767e599cc0
othmane-boudiab <mrboudiabothmane@gmail.com>
2022-04-19 11:37:25 +0000

    modifie design system tailwind

--- WDCManager_frontend/tailwind.config.js
+++ WDCManager_frontend/tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainbleu : "#0E74FA",
+        bleudark : "#091D51",
+        mainbg : "#F1F4F9",
+        secondarybleu : "#183787",
+      }
+    },
   },
   plugins: [],
 }
